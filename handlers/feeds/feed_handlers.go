964c9d74ca49b2028785a3d0a6b16e272b22fcf0
kobeld <kobeld@163.com>
2013-07-02 21:43:45 +0800

    Add something to the home pages

--- handlers/feeds/feed_handlers.go
+++ handlers/feeds/feed_handlers.go
@@ -1,13 +1,35 @@
 package feeds
 
 import (
+	"github.com/kobeld/duoerl/services"
+	"github.com/kobeld/duoerlapi"
 	. "github.com/paulbellamy/mango"
 	"github.com/sunfmin/mangotemplate"
 )
 
+type HomeViewData struct {
+	ApiProducts []*duoerlapi.Product
+	ApiBrands   []*duoerlapi.Brand
+}
+
 func Index(env Env) (status Status, headers Headers, body Body) {
 
-	mangotemplate.ForRender(env, "feeds/index", nil)
+	apiProducts, err := services.AllProducts()
+	if err != nil {
+		panic(err)
+	}
+
+	apiBrands, err := services.AllBrands()
+	if err != nil {
+		panic(err)
+	}
+
+	homeViewData := &HomeViewData{
+		ApiProducts: apiProducts,
+		ApiBrands:   apiBrands,
+	}
+
+	mangotemplate.ForRender(env, "feeds/index", homeViewData)
 
 	return
 }
